23ca048e4d6bf2bc327d309a02ab18e269f357e5
Fer C <fjchaure@gmail.com>
2015-07-24 20:21:37 -0300

    improve matlab tool

--- tools/iibm_loader.m
+++ tools/iibm_loader.m
@@ -29,64 +29,65 @@ classdef iibm_loader()
         end
         
         function data = get_data(channels,beg_time, final_time)
+        %
+		% Usage:
+		%       channels is a vector from 1 to the data_channels,
+		% with the selected channels
+        
             switch nargin
                  case 0
                      channels = 'all';
                      beg_time = 0;
-                     final_time= 'all';
+                     final_time= 'end';
                  case 1
-                      beg_time = 0;
-                     final_time= 'all';
+                     beg_time = 0;
+                     final_time= 'end';
                  case 2
-                     final_time= 'all';
+                     final_time= 'end';
+            end
+            
+            if total_samples < beg_sample
+				error('beg_time out of data.')
             end
             if strcmp(channels,'all')
                channels = [1:data_channels];
-            if strcmp(final_time,'all')
+            end
+            if strcmp(final_time,'end')
                final_time = total_samples;    
+            end 
+            if final_sample <= beg_sample
+				error('final < beginning.')
+			end
+            final_file = floor(final_sample/samples4file);
+            rel_final_sample = final_sample - final_file * samples4file;
+            data = zeros(size(channels,2),final_sample-beg_sample)
+            if beg_file != final_file
+				%inicio:
+				fid = fopen(strcat(reg_filename,"-",num2str(beg_file+1)),'r'); #plus 1, because data began in 1
+				data(:, 1:samples4file - rel_beg_sample) = fread(fid,[data_channels,samples4file],'int16')(channels,rel_beg_sample:end);
+				writed_samples = samples4file - rel_beg_sample;
+				fclose(fid);
+				
+				#final
+				fid = fopen(strcat(reg_filename, "-", num2str(final_file + 1)), 'r'); #plus 1, because data began in 1
+				data(:, end - rel_final_sample:end) = fread(fid, [data_channels,samples4file], 'int16')(channels,1:rel_final_sample);
+				fclose(fid);
+				
+				for i = [beg_file+1,final_file+1]
+					fid = fopen(strcat(reg_filename, "-", num2str(i + 1)), 'r'); #plus 1, because data began in 1
+					data(:, writed_samples:writed_samples+samples4file) = fread(fid,[data_channels,samples4file],'int16')(channels,:);
+					fclose(fid);
+				end
+				
+            else
+				fid = fopen(strcat(reg_filename, "-", num2str(beg_file + 1)), 'r'); #plus 1, because data began in 1
+                data = fread(fid,[data_channels,samples4file],'int16')(channels,rel_beg_sample:rel_final_sample);          
             
-            final_file = floor(final_sample/samples4file)
-            rel_final_sample = final_sample - final_file * self.samples4file
-            data = np.ndarray([len(channels),final_sample-beg_sample])
-           
-           
-           %first block
-            fid=fopen([reg_filename '_' num2str(n)],'r');
-            data=fread(fid,[25,samples4file],'int16');
-            canal=[canal data(4*(tetrodo_nro-1)+canal_nro,:)];
-            strobe=[strobe data(25,:)];
-            fclose(fid);
+            end
+
         end
         
     end
 end
 
 
-
-
-
-fname='240112';					%%%% Nombre de la sesion
-for tetrodo_nro=1				%%%% la info esta agrupada por tetrodos y canales, cada tetrodo tiene 4 canales
-    for canal_nro=2				%%%% si sequiere extraen el canal 10 por ejemplo, tetrodo_nro=3 y canal_nro=9
-        canal=[];
-        strobe=[];
-        file_tot=213;				%%%% Poner aca la cantidad de archivos (no incluir el header)
-        file_qty=10;				%%%% dejar este numero fijo al menos que haya muuuucha memoria
-        file_length=801000;			%%%% longitud de cada archivo en bytes
-
-        canal_filt_sm=[];ini=[];fin=[];
-        L(1)=0;
-        for k=1:file_qty:file_tot
-            k
-            canal=[];strobe=[];
-            for n=k:min(file_tot,k+file_qty-1)
-                fid=fopen([reg_filename '_' num2str(n)],'r');
-                data=fread(fid,[25,samples4file],'int16');
-                canal=[canal data(4*(tetrodo_nro-1)+canal_nro,:)];
-                strobe=[strobe data(25,:)];
-                fclose(fid);
-            end
-        end
-
-    end
-end
